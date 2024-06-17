be9da5fb2b34a473d19a2449606b6bcdd0740015
shangye10159 <shangye1015@gmail.com>
2024-06-17 17:23:02 +0800

    [Refactor]优化规范代码，去掉报警

--- flink-learning-basic/flink-learning-data-sinks/src/main/java/com/zhisheng/data/sinks/utils/KafkaUtil.java
+++ flink-learning-basic/flink-learning-data-sinks/src/main/java/com/zhisheng/data/sinks/utils/KafkaUtil.java
@@ -11,21 +11,22 @@ import java.util.Properties;
  * Desc: 往kafka中写数据,可以使用这个main函数进行测试
  * Created by zhisheng on 2019-02-17
  * Blog: http://www.54tianzhisheng.cn/tags/Flink/
+ * @author tianzhisheng
  */
 public class KafkaUtil {
-    public static final String broker_list = "localhost:9092";
-    public static final String topic = "student";  //kafka topic 需要和 flink 程序用同一个 topic
+    public static final String BROKER_LIST = "localhost:9092";
+    public static final String TOPIC = "student";  //kafka topic 需要和 flink 程序用同一个 topic
 
     public static void writeToKafka() throws InterruptedException {
         Properties props = new Properties();
-        props.put("bootstrap.servers", broker_list);
+        props.put("bootstrap.servers", BROKER_LIST);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        KafkaProducer producer = new KafkaProducer<String, String>(props);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
         for (int i = 1; i <= 100; i++) {
             Student student = new Student(i, "zhisheng" + i, "password" + i, 18 + i);
-            ProducerRecord record = new ProducerRecord<String, String>(topic, null, null, GsonUtil.toJson(student));
+            ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, null, null, GsonUtil.toJson(student));
             producer.send(record);
             System.out.println("发送数据: " + GsonUtil.toJson(student));
         }
